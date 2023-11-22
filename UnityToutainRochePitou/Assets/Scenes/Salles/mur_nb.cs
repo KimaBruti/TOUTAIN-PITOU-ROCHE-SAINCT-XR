bd07ba3ef7ea62cf6319d46f84400add3b4e6c10
KimaBruti <150810546+KimaBruti@users.noreply.github.com>
2023-11-22 14:34:12 +0100

    ajout du labyrinthe
    ajout du code de Tom sur le lab

---
+++ UnityToutainRochePitou/Assets/Scenes/Salles/mur_nb.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mur_nb : MonoBehaviour
+{
+
+    public int nb_mur;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
